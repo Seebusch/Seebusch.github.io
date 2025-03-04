68c9359c91c9ca13e774ca252fd8604788a25720
Sebastian 'Seebusch' Szczepaniak <67638465+Seebusch@users.noreply.github.com>
2025-03-04 22:11:43 +0100

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-class RedirectHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(302)
-        self.send_header("Location", "http://localhost:5001/admin")
-        self.end_headers()
-
-HTTPServer(("0.0.0.0", 8080), RedirectHandler).serve_forever()
