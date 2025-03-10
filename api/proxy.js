bf549242de8143ac40031c90eebb19f29653924f
Sebastian 'Seebusch' Szczepaniak <67638465+Seebusch@users.noreply.github.com>
2025-03-10 23:15:06 +0100

    SSRF CTF Try

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
     try {
         const response = await axios.get('http://localhost:5000/admin', {
             headers: {
-                'Host': 'fasttravel'
+                'Host': 'localhost'
             }
         });
         res.send(response.data);
